70b774b3e3d66d4d9343d242ffe0aa54a2fc3bdf
vivianzmyff <vivz0304@gmail.com>
2025-10-30 18:56:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // Ensure correct paths when deploying to GitHub Pages at /AFrame-Test/
+  base: '/AFrame-Test/',
   server: {
     host: true,
     port: 3000
